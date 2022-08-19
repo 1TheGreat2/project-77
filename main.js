6c1b38a6c1e612d29d144c45243b17de830a3dfd
1TheGreat2 <83286527+1TheGreat2@users.noreply.github.com>
2022-08-19 20:30:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Ryan"
+second_name = " Saket"
+a = 10
+b = 5
+var add = a + b
+console.log(add)
+console.log(first_name.concat(second_name))
+two = 2
+three = 3
+twentyfour = 24
+twentysix = 26
+one = 1
+add1 = two + three
+add2 = twentyfour + twentysix
+add3 = one + one
+console.log (add1)
+console.log (add2)
+console.log (add3)
